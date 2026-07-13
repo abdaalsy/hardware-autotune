49c1433bfb6b9b87962a6797205e304b161e416d
abdaalsy <abdaalsy@gmail.com>
2026-07-12 20:49:03 -0400

    nearest note function

--- research/autotune.py
+++ research/autotune.py
@@ -5,6 +5,7 @@ import numpy as np
 import argparse
 import sounddevice
 from pitch_detection import detect_pitch
+from note_matching import nearest_note
 
 def stream_wav_file(file_path, chunk_size=512):
     # Open the WAV file
@@ -51,19 +52,20 @@ parser.add_argument("-i", "--input", default="vocals.wav", type=str, help="Input
 parser.add_argument("-s", "--shift", type=float, help="(DEBUG) Shift amount value")
 args = parser.parse_args()
 
-SAMPLE_RATE = 44100
-TAU_MIN = int(44100/800)
-TAU_MAX = 1024   # close enough to int(44100/50)
-WINDOW_SIZE = 2048      # close enough to 2* int(44100/50). Larger is better
+SAMPLE_RATE = 48000
+TAU_MIN = int(48000/800)
+TAU_MAX = 1024   # close enough to int(48000/50)
+WINDOW_SIZE = 2048      # close enough to 2* int(48000/50). Larger is better
 BLOCK_SIZE = 2048 
-IN_BUFFER_LEN = 4096*2
-THRESHOLD_PITCH = 0.1
+IN_BUFFER_LEN = 8192
+THRESHOLD_PITCH = 0.15
 
 input_stream = stream_wav_file(args.input, BLOCK_SIZE)
 input_buffer = np.zeros(IN_BUFFER_LEN)
 output_buffer = np.zeros(BLOCK_SIZE)
 read_pos = 0
 write_pos = read_pos + BLOCK_SIZE
+old_pitch = 0
 
 def check_overtake(pointer1, pointer2, jump_size, buffer_length):
     # Returns true if pointer1 is about to overtake pointer2 after stepping forward jump_size elements in a circular buffer of length buffer_length
@@ -92,14 +94,14 @@ def write_head():
     write_pos %= len(input_buffer)
 
 def read_head():
-    global read_pos, output_buffer
+    global read_pos, output_buffer, old_pitch
     # Get the pitch from the current write_pos going back
     pitch_indexes = np.arange(write_pos, write_pos + len(input_buffer), dtype=np.int32) % len(input_buffer)
     pitch = detect_pitch(input_buffer[pitch_indexes], SAMPLE_RATE, WINDOW_SIZE, TAU_MAX, TAU_MIN, THRESHOLD_PITCH)
-    period_samples = SAMPLE_RATE / 160
-    if pitch:
-        period_samples = SAMPLE_RATE / pitch
-    print(pitch)
+    if not pitch:
+        pitch = 150
+    period_samples = SAMPLE_RATE / pitch
+
     
     # Write to output buffer, stepping read head
     shift = args.shift
@@ -117,17 +119,17 @@ def read_head():
         real_read_pos %= len(input_buffer)
         if check_overtake(read_pos, write_pos, 2, len(input_buffer)):
             read_pos -= period_samples
-            real_read_pos -= period_samples*shift
+            real_read_pos -= period_samples
             read_pos %= len(input_buffer)
             real_read_pos %= len(input_buffer)
         elif check_overtake(write_pos, read_pos, BLOCK_SIZE, len(input_buffer)):
             read_pos += period_samples
-            real_read_pos += period_samples*shift
+            real_read_pos += period_samples
             read_pos %= len(input_buffer)
             real_read_pos %= len(input_buffer)
 
     output_buffer = input_buffer[read_indexes]
-    return pitch
+    old_pitch = pitch
 
 def audio_callback(outdata, frames, time_info, status):
     # This function is called by sounddevice in a separate background thread every time the audio buffer needs new data
--- research/note_matching.py
+++ research/note_matching.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 # The notes in Major keys follow the pattern:   W - W - H - W - W - W - H
 # Minor keys follow:                            W - H - W - W - H - W - W
@@ -7,11 +8,66 @@ import numpy as np
 
 # Human voice ranges from 50-800 Hz. That's from G1 to A5, 
 # If we're supplied with a key, we should assemble a cache with all the notes for that key for as many octaves that fit in the range
-# The plan is, given a root note and an indication for major/minor:
-#   - Use the major/minor pattern to determine the notes
-#   - Use a LUT to convert each to frequencies.
-#   - Extend until we fill 50-800 Hz range (next octave is at double frequency)
-# After we're complete, we'll have a table of length which is a multiple of 8.
 
-def generate_freq_table(root_note, major, min_hz, max_hz):
+def generate_freq_table(root_note, major, max_hz):
+    FREQ_MINS = {
+        "C": 32.70,
+        "CS": 34.65,
+        "D": 36.71,
+        "DS": 38.89,
+        "E": 41.20,
+        "F": 43.65,
+        "FS": 46.25,
+        "G": 49.00,
+        "GS": 51.91,
+        "A": 55.00,
+        "AS": 58.27,
+        "B": 61.74
+    }
+
+    W_RATIO = float(2**(2.0/12.0))
+    H_RATIO = float(2**(1.0/12.0))
+
+    MAJOR_JUMPS = [W_RATIO, W_RATIO, H_RATIO, W_RATIO, W_RATIO, W_RATIO, H_RATIO]
+    MINOR_JUMPS = [W_RATIO, H_RATIO, W_RATIO, W_RATIO, H_RATIO, W_RATIO, W_RATIO]
+    
+    table = []
+    current_freq = FREQ_MINS[root_note]
+    i = 0
+    while current_freq < max_hz:
+        if i > 6:
+            i = 0   # We could also % by 7 but I think thats more work, however it looks simpler
+        table.append(current_freq)
+        if major:
+            current_freq *= MAJOR_JUMPS[i]
+        else:
+            current_freq *= MINOR_JUMPS[i]
+        i += 1
+
+    return table
+
+def find_nearest_note(freq, table):
+    # binary search since table will be sorted, return last value
+    start = 0
+    end = len(table)-1
+    while abs(start - end) > 1:
+        print(f"({start}, {end})")
+        mid = int((start + end)/2)
+        if freq >= table[mid]:
+            start = mid
+        else:
+            end = mid
+    
+    if start != end:
+        diff = abs(table[start] - freq)
+        if abs(table[end] - freq) < diff:
+            return table[end]
+        else:
+            return table[start]
+    else:
+        return table[start]
+
+if __name__ == "__main__":
+    table = generate_freq_table(sys.argv[1], sys.argv[2].lower() == "major", 800)
+    print(find_nearest_note(100, table))
 
